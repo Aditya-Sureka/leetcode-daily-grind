34145a8d43671e3772768ba4ce3b62dcc4e17aad
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-07-03 19:15:14 +0530

    Time: 11 ms (21.68%) | Memory: 57.2 MB (65.59%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        StringBuilder sb = new StringBuilder();
+        int X = 0;
+
+        for(int shift : shifts) {
+            X = (X + shift) % 26;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            sb.append((char)((index + X) % 26 + 97));
+            X = Math.floorMod(X - shifts[i], 26);
+            
+        }
+        return sb.toString();
+        
+    }
+}
