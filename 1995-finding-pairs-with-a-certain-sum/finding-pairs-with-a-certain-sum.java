185ca23767defbef3c151059580e1da1e3d9ac75
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-07-06 14:52:35 +0530

    Time: 179 ms (13.33%) | Memory: 80.5 MB (14.03%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
@@ -1,34 +1,32 @@
 class FindSumPairs {
     int[] a, b;
+    Map<Integer, Integer> uma = new HashMap<>();
     Map<Integer, Integer> umb = new HashMap<>();
-    int mx = 0;
 
     public FindSumPairs(int[] nums1, int[] nums2) {
-        a = nums1.clone();
+        a = nums1;
         b = nums2;
-        Arrays.sort(a);
 
-        for(int val : b) {
-            umb.put(val, umb.getOrDefault(val, 0) + 1);
-            mx = Math.max(mx, val);
-        }
+        for (int num : nums1)
+            uma.put(num, uma.getOrDefault(num, 0) + 1);
+
+        for (int num : nums2)
+            umb.put(num, umb.getOrDefault(num, 0) + 1);
     }
-    
+
     public void add(int index, int val) {
         umb.put(b[index], umb.get(b[index]) - 1);
         b[index] += val;
-        mx = Math.max(mx, b[index]);
         umb.put(b[index], umb.getOrDefault(b[index], 0) + 1);
     }
-    
-    public int count(int tot) {
-        int pairs  = 0;
-        int start = Arrays.binarySearch(a, tot - mx - 1);
-        if(start < 0) start = 0;
 
-        for(int i = start; i < a.length; i++) {
-            int remain = tot - a[i];
-            pairs += umb.getOrDefault(remain, 0);
+    public int count(int tot) {
+        int pairs = 0;
+        for (Map.Entry<Integer, Integer> entry : uma.entrySet()) {
+            int val = entry.getKey();
+            int freq = entry.getValue();
+            int remain = tot - val;
+            pairs += freq * umb.getOrDefault(remain, 0);
         }
         return pairs;
     }
