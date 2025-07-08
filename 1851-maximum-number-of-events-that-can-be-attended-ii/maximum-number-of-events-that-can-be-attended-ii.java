018d26b71e5c35f260d8e31ac55ee502426302e0
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-07-08 20:04:29 +0530

    Time: 66 ms (61.54%) | Memory: 140 MB (11.54%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,5 +1,35 @@
 class Solution {
-    public static int bisectRight(int[][] events, int target) { // Approach 2: Bottom-up Dynamic Programming + Binary Search
+    public int maxValue(int[][] events, int k) { // Approach 3: Top-down Dynamic Programming + Cached Binary Search
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        int n = events.length;
+        nextIndices = new int[n];
+        for (int curIndex = 0; curIndex < n; ++curIndex) {
+            nextIndices[curIndex] = bisectRight(events, events[curIndex][1]);
+        }
+        
+        dp = new int[k + 1][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return dfs(0, k, events);
+    }    
+    
+    private int[][] dp;
+    private int[] nextIndices;
+    private int dfs(int curIndex, int count, int[][] events) {
+        if (count == 0 || curIndex == events.length) {
+            return 0;
+        }
+        if (dp[count][curIndex] != -1) {
+            return dp[count][curIndex];
+        }
+        int nextIndex = nextIndices[curIndex];
+        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
+        return dp[count][curIndex];
+    }
+    
+    public static int bisectRight(int[][] events, int target) { 
         int left = 0, right = events.length;
         while (left < right) {
             int mid = (left + right) / 2;
@@ -10,19 +40,5 @@ class Solution {
             }
         }
         return left;
-    } 
-    
-    public int maxValue(int[][] events, int k) {
-        int n = events.length;
-        int[][] dp = new int[k + 1][n + 1];
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-
-        for (int curIndex = n - 1; curIndex >= 0; --curIndex) {
-            for (int count = 1; count <= k; count++) {
-                int nextIndex = bisectRight(events, events[curIndex][1]);
-                dp[count][curIndex] = Math.max(dp[count][curIndex + 1], events[curIndex][2] + dp[count - 1][nextIndex]);
-            }
-        }
-        return dp[k][0];
-    }
+    }   
 }
