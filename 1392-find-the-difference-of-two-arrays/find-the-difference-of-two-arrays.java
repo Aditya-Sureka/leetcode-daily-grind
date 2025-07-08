b82e32b16fa6fc0a31a87f80cd21bb459cffcbdf
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-07-09 01:46:54 +0530

    Time: 10 ms (77.84%) | Memory: 45.4 MB (75.83%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,27 +1,20 @@
 class Solution {
-    // Returns the elements in the first arg nums1 that don't exist in the second arg nums2.
-    List<Integer> getElementsOnlyInFirstList(int[] nums1, int[] nums2) {
-        Set<Integer> onlyInNums1 = new HashSet<> (); 
-        
-        // Store nums2 elements in an unordered set. 
-        Set<Integer> existsInNums2 = new HashSet<> (); 
-        for (int num : nums2) {
-            existsInNums2.add(num);
-        }
-        
-        // Iterate over each element in the list nums1.
-        for (int num : nums1) {
-            if (!existsInNums2.contains(num)) {
-                onlyInNums1.add(num);
-            }
-        }
-        
-        // Convert to vector.
-        return new ArrayList<>(onlyInNums1);
-    }
-    
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return Arrays.asList(getElementsOnlyInFirstList(nums1, nums2), 
-        getElementsOnlyInFirstList(nums2, nums1));
-    }
+	public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+		Set<Integer> set1 = new HashSet<>();                           // create 2 hashsets
+		Set<Integer> set2 = new HashSet<>();
+		for(int num : nums1){ set1.add(num); }                         // add nums1 elements to set1
+		for(int num : nums2){ set2.add(num); }                         // add nums2 elements to set2
+		
+		List<List<Integer>> resultList = new ArrayList<>();             // Initialize result list with 2 empty sublists that we will return
+		resultList.add(new ArrayList<>());
+		resultList.add(new ArrayList<>());
+
+		for(int num : set1){                                            // just iterate to all elements of set1
+			if(!set2.contains(num)){ resultList.get(0).add(num); }      // add those elements to first sublist of result list, which are not in set2.
+		}
+		for(int num : set2){                                            // just iterate to all elements of set2
+			if(!set1.contains(num)){ resultList.get(1).add(num); }      // add those elements to first sublist of result list, which are not in set1
+		}
+		return resultList;
+	}
 }
