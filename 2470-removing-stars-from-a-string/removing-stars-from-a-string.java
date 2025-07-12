53f95474b8acdfa4f334e5abcced5f110a2eb839
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-07-12 23:49:13 +0530

    Time: 60 ms (63.27%) | Memory: 46 MB (56.47%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,13 +1,17 @@
 class Solution {
     public String removeStars(String s) {
-        StringBuilder st = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '*') {
-                st.deleteCharAt(st.length() - 1);
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '*') {
+                st.pop();
             } else {
-                st.append(s.charAt(i));
+                st.push(c);
             }
         }
-        return st.toString();
+        StringBuilder sb = new StringBuilder();
+        for(char c : st) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
