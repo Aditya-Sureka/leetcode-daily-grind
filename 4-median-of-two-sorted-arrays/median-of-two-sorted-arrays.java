d6305185b528a7681848fb9ca19aa507fca15c37
Aditya Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2025-12-13 15:56:02 +0530

    Time: 1 ms (99.84%) | Memory: 48.6 MB (61.22%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,31 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] ans = new int[m + n];
+      // Make sure array a is the smaller one to minimize binary search range
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
 
-        for(int i = 0; i < m; i++) {
-            ans[i] = nums1[i];
+        int n1 = nums1.length, n2 = nums2.length;
+        int low = 0, high = n1;
+
+        // Binary search on the smaller array
+        while (low <= high) {
+            // Calculate cut points for both arrays
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            // Get values to the left and right of the partition
+            // Use Integer.MIN_VALUE and MAX_VALUE to handle edge cases
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // If partition is valid
+            if (l1 <= r2 && l2 <= r1) {
+                // Even total elements => average of two middle elements
+                if ((n1 + n2) % 2 == 0)
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                else
+                    // Odd total elements => max of left parts
+                    return Math.max(l1, l2);
+            }
+            // Move left in array a
+            else if (l1 > r2)
+                high = cut1 - 1;
+            // Move right in array a
+            else
+                low = cut1 + 1;
         }
 
-        for(int j = 0; j < n; j++) {
-            ans[m + j] = nums2[j];  
-        } 
-
-        Arrays.sort(ans);
-
-        int k = ans.length;
-
-        if(k == 1) return ans[0]; 
-
-        double median = 0;
-
-        if(k % 2 == 1) { 
-            median = ans[k / 2];  
-        } else {
-            median = (ans[k/2] + ans[(k/2) - 1]) / 2.0; 
-        }
-
-        return median;
+        return 0.0; 
     }
 }
